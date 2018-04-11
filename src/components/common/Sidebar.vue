58d5f536ac69c0390df0e51e743adac2cfb3ad14
yexiuqing <1124934298@qq.com>
2018-04-11 14:44:40 +0800

    update sidebar

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -17,15 +17,21 @@
       </el-menu>
       <el-menu :default-openeds="['1', '3']" style="min-height: 100%;" background-color="#fff" text-color="#666" router>
         <el-menu-item index="examLogin">
-          <template slot="title"><i class="el-icon-message"></i>在线考试</template>
+          <template slot="title"><i class="el-icon-message"></i>发布考试</template>
         </el-menu-item>
         <el-menu-item index="onlinePractice">
-          <template slot="title"><i class="el-icon-menu"></i>在线练习</template>
+          <template slot="title"><i class="el-icon-menu"></i>导入学生名单</template>
         </el-menu-item>
         <el-menu-item index="searchScore">
-          <template slot="title"><i class="el-icon-setting"></i>成绩查询</template>
+          <template slot="title"><i class="el-icon-setting"></i>主观题判分</template>
         </el-menu-item>
          <el-menu-item index="talkAbout">
+          <template slot="title"><i class="el-icon-setting"></i>重难点分析</template>
+        </el-menu-item>
+        <el-menu-item index="talkAbout">
+          <template slot="title"><i class="el-icon-setting"></i>成绩查询</template>
+        </el-menu-item>
+        <el-menu-item index="talkAbout">
           <template slot="title"><i class="el-icon-setting"></i>留言交流</template>
         </el-menu-item>
       </el-menu>
