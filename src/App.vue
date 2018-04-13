fb808115ca7c38a43666b75052248eaafbc8ba7e
yexiuqing <1124934298@qq.com>
2018-04-13 16:22:12 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
   <div id="app">
-     <router-view/>
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
 <script>
-// import Header from "./components/common/Header.vue"
-// import Sidebar from "./components/common/Sidebar.vue"
 export default {
-  name: "App",
-  // components: {
-  //   'v-header': Header,
-  //   'v-sidebar': Sidebar
-  // }
-};
+  name: 'App'
+}
 </script>
 
-<style scoped lang="scss">
-body{
-  margin: 0;
-}
+<style>
 #app {
-  color: #666;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,18 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import store from './store/index'
 
-Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  store,
   components: { App },
   template: '<App/>'
 })
